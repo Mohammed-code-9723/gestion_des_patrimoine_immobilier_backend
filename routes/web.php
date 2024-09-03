7b5350c765c5b14c878a04576950731f62f4ebbe
Mohammed-code-9723 <mohameddhimni311@gmail.com>
2024-09-03 16:09:44 +0100

    last version 03/09/2024 16:09

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/{path?}', function () {
+    return file_get_contents(public_path('react-build/index.html'));
+})->where('path', '.*');
