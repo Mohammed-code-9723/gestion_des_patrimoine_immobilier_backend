d9a95c939c18e4b358e6954eb7a0c3fd5b788476
Mohammed-code-9723 <mohameddhimni311@gmail.com>
2024-09-08 16:51:34 +0100

    08/09/2024

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/{path?}', function () {
-    $indexPath = public_path('react-build/index.html');
-
-    if (file_exists($indexPath)) {
-        return response()->file($indexPath, [
-            'Content-Type' => 'text/html',
-        ]);
-    } else {
-        abort(404, 'File not found');
-    }
-})->where('path', '.*');
